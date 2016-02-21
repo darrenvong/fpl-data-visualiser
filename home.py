527216afab98fdfecfabbd2fb85c932db690a4d1
Darren Vong <d.vongy@gmail.com>
2016-02-21 20:06:46 +0000

    Working pound stretcher function in the model/logic level

--- home.py
+++ home.py
@@ -18,8 +18,20 @@ def get_hot_players(col):
     return [player for player in cursor]
 
 def pound_stretchers(col):
-    pass
+    query = {"total_points": {"$gt": 0}}
+    projection = {"_id": 0, "now_cost": 1, "total_points": 1}
+    cursor = col.find(query, projection)
+    values, points = [], []
+    for player in cursor:
+        values.append(player["now_cost"])
+        points.append(player["total_points"])
+    print "Total active player value:", values
+    print "Total active player points:", points
+    num_of_players = len(values)
+    print num_of_players
+    return sum(values)/num_of_players, sum(points)/num_of_players
 
 _, players = connect()
-players_list = get_hot_players(players)
-print players_list
+# players_list = get_hot_players(players)
+average_value = pound_stretchers(players)
+print average_value
