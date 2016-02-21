5124eecfb6fcdfbf8f573ce9404ac56d862e5c21
Darren Vong <d.vongy@gmail.com>
2016-02-21 21:26:13 +0000

    Back-end functions for populating the home page tables are now all done

--- home.py
+++ home.py
@@ -4,6 +4,7 @@ Created on 21 Feb 2016
 @author: Darren
 """
 from pymongo import MongoClient
+from bson import SON
 import pymongo
 
 def connect():
@@ -18,20 +19,30 @@ def get_hot_players(col):
     return [player for player in cursor]
 
 def pound_stretchers(col):
+    avg_points = round(find_key_averages(col))
+    avg_cost = 50
+    query = {"now_cost": {"$lte": avg_cost}, "total_points": {"$gte": avg_points}}
+    projection = {"_id": 0, "web_name": 1, "team_name": 1, "now_cost": 1, "total_points": 1}
+    cursor = col.find(query, projection, sort=[("total_points", pymongo.DESCENDING)], limit=5)
+    return [player for player in cursor]
+        
+def find_key_averages(col):
+    """Finds the average points and values of all active players in Fantasy Premier League"""
     query = {"total_points": {"$gt": 0}}
-    projection = {"_id": 0, "now_cost": 1, "total_points": 1}
-    cursor = col.find(query, projection)
-    values, points = [], []
-    for player in cursor:
-        values.append(player["now_cost"])
-        points.append(player["total_points"])
-    print "Total active player value:", values
-    print "Total active player points:", points
-    num_of_players = len(values)
-    print num_of_players
-    return sum(values)/num_of_players, sum(points)/num_of_players
+    projection = {"_id": 0, "avg_points": 1}
+    pipeline = [{"$match": query}, {"$group": {"_id": None, "avg_points": {"$avg": "$total_points"}}},
+                {"$project": projection}]
+    cursor = col.aggregate(pipeline)
+    return cursor.next()["avg_points"]
+
+def most_popular(col):
+    projection = {"_id": 0, "web_name": 1, "team_name": 1, "now_cost": 1, "total_points": 1,
+                  "net_transfers": {"$subtract": ["$transfers_in_event", "$transfers_out_event"]}}
+    pipeline = [{"$project": projection}, {"$sort": SON([("net_transfers", -1)])},
+                {"$limit": 5}]
+    cursor = col.aggregate(pipeline)
+    return [player for player in cursor]
 
 _, players = connect()
 # players_list = get_hot_players(players)
-average_value = pound_stretchers(players)
-print average_value
+print most_popular(players)
--- router.py
+++ router.py
@@ -5,23 +5,19 @@ Created on 21 Feb 2016
 from bottle import Bottle, static_file, template, redirect
 
 class Router(Bottle):
-    """
-    This class is responsible for directing requests to callback handler functions
-    and subsequently return the appropriate contents.
-    """
+    """This class is responsible for directing requests to callback handler functions
+    and subsequently return the appropriate contents."""
 
     def __init__(self, *args, **kwargs):
-        """
-        Starts the visualiser application.
-        """
+        """Starts the visualiser application."""
+        
         super(Router, self).__init__(*args, **kwargs)
         self._route()
     
     def _route(self):
-        """
-        A pseudo-private method for binding callback functions to the different
-        end points (i.e. URL paths) of the application.
-        """
+        """A pseudo-private method for binding callback functions to the different
+        end points (i.e. URL paths) of the application."""
+        
         self.route("/", callback=self.re_route)
         self.route("/index", callback=self.root)
         self.route("<path:path>", callback=lambda path: self.get_resources(path))
